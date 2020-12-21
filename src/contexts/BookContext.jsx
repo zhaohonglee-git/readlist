1070fdda8758a2b3f4edb1fff3bd334ad685b9d2
zhaohonglee <296222105@qq.com>
2020-12-21 11:53:49 +0800

    loacalStroage 保存数据 last commit

--- src/contexts/BookContext.jsx
+++ src/contexts/BookContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react'
 import { BookReducer } from '../reducers/BookReducer'
 
 export const BookContext = createContext()
@@ -7,7 +7,16 @@ const BookCotextProvider = (props) => {
   const [books, dispatch] = useReducer(BookReducer, [
     { title: '三体', author: '刘慈欣', id: 1 },
     { title: '红楼梦', author: '曹雪芹', id: 2 }
-  ])
+  ], () => {
+    const result = localStorage.getItem('books')
+    return (
+      result ? (JSON.parse(result)) : ([])
+    )
+  })
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books))
+  }, [books])
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
@@ -24,4 +33,5 @@ export default BookCotextProvider
 - useReducer接收两个参数：BookReducer 初始state数据值，得到books数据值和dispatch函数
 - dispatch函数就是用来解决不同情况需要执行的函数代码集问题
 - 下一步请看BookReducer组件
+- 利用useEffect来实现localStorage的数据存储,为了逻辑严谨，对原始的数据做了判断
 */
